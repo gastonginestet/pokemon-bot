ff362b1e4c9dc8ad336835fba63934628f4774c4
julianfere <julianferegotti96@gmail.com>
2021-02-08 04:06:58 -0300

    Rubocop offences and more information to the embed

--- pokemon_bot.rb
+++ pokemon_bot.rb
@@ -1,14 +1,22 @@
-require "discordrb"
+require 'discordrb'
 require 'rest-client'
 require 'json'
 
-def tell_me_a_random_pokemon ()
+def tell_me_a_random_pokemon
   response = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{rand 251}")
   results = JSON.parse(response.to_str)
   name = (results['forms'][0]['name']).capitalize!
   image = results['sprites']['front_default']
-  arr = [name,image]
-  return arr
+  type = results['types'][0]['type']['name']
+  id = results['id']
+  [name, image, type, id]
+end
+
+def get_colour(type)
+  File.open('types.json', 'r') do |file|
+    colours = JSON.parse(file.read)
+    colours[type]
+  end
 end
 
 @my_bot = Discordrb::Bot.new token: ENV['TOKEN']
@@ -20,9 +28,12 @@ end
 @my_bot.message(with_text: 'decime algun pokemon') do |event|
   arr = tell_me_a_random_pokemon()
   event.channel.send_embed do |embed|
-    embed.title = "#{arr[0]}"
-    embed.colour = 0xcc0000
-    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{arr[1]}")
+    embed.author = { name: "ID: #{arr[3]}" }
+    embed.title = arr[0].to_s
+    embed.description = "Type: #{arr[2].to_s.capitalize}"
+    embed.colour = get_colour(arr[2])
+    embed.footer = { text: '«PokeBot»' }
+    embed.image = Discordrb::Webhooks::EmbedImage.new(url: arr[1].to_s)
   end
 end
 
